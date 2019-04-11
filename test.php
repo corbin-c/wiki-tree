3147dd7c52bfa5882316c64fb491caa5f3ac7e07
tougodo <corbin.clement@gmail.com>
2019-04-11 14:48:35 +0200

    clean dat mess

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("./array.php");
-end($donnees);
-var_dump(current($donnees));
-?>
--- wiki.php
+++ wiki.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 <?php
-
+ini_set('memory_limit', '1024M');
+include("./array.php");
 $n=0;
 function wikiquery($s) {
 	global $n;
@@ -26,24 +27,49 @@ function wikiquery($s) {
 	$data = $data["query"]["categorymembers"];
 	foreach($data as $cle => $element)
 	{	
-		if ($element["ns"]==14 && $n <= 10)
+		if ($element["ns"]==14 && $n <= 50)
 		{
 			$data[$cle]["catfille"]=wikiquery($element["title"]);
-			echo $element["title"];
-			echo "<br/>";
+			//echo $element["title"];
+			//echo "<br/>";
 		}
 		if ($cle+1==count($data)) //on est au bout d'une branche
 		{
 			$n=$n+1;
-			echo $n;
-			echo "<br/>";
+			//echo $n;
+			//echo "<br/>";
 		}
 	}
 	return $data;
 }
+function continuer($tableau) {
+	global $n;
+	foreach($tableau as $cle => $element)
+	{
+		if ($element["ns"]==14)
+		{
+			if (!isset($element["catfille"]))
+			{
+				$tableau[$cle]["catfille"] = wikiquery($tableau[$cle]["title"]);
+				//$m++;
+				//echo $tableau[$cle]["title"];
+				//echo "<br/>";
+				
+			}
+			else
+			{
+				$tableau[$cle]["catfille"] = continuer($element["catfille"]);
+			}			
+		}
+
+	}
+	return $tableau;
+}
 echo "<pre>";
-var_export(wikiquery("Catégorie:Musique"));
+
+var_export(continuer($donnees));
 echo "</pre>";
+//continuer($donnees);
 ?>
 </body>
 </html>
