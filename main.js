a30e354c42939ed58894bbb4ac97f23b6c629e18
tougodo <corbin.clement@gmail.com>
2019-06-07 13:37:12 +0200

    working with d3

--- main.js
+++ main.js
@@ -31,13 +31,16 @@ force_graph.onmessage = function (event) {
 		.attr("stroke-opacity", "1")
 			
 	var node = svg_nodes.selectAll("g").data(event.data.nodes, function(d) { return d.name });
+
 	node.exit().transition().duration(2*duration/3)
 		.style("opacity",0)
 		.remove();
+
 	node.select("circle").style("opacity", 1).transition().duration(duration)
-			.attr("cy", function(d) { return d.y })
-			.attr("cx", function(d) { return d.x })
-			.attr("r",  5)
+		.attr("cy", function(d) { return d.y })
+		.attr("cx", function(d) { return d.x })
+		.attr("r",  5)
+
 	node.select("text")
 		.transition().duration(duration)
 		.attr("x", function(d) { return d.x - this.getComputedTextLength() / 2} )
@@ -54,6 +57,7 @@ force_graph.onmessage = function (event) {
 			.attr("x", function(d) { return d.x - this.getComputedTextLength() / 2} )
 			.attr("y", function(d) { return d.y +10})
 			.style("opacity",0).transition().delay(duration).duration(duration).style("opacity", 1)
+	
 	nu.append("circle")
 		.attr("cx", function(d) { if ((typeof d.parent !== 'undefined') && (typeof d.parent !== 'boolean')) { if (typeof positions[d.parent] === 'undefined') { xorigin = d3.select("#id"+d.parent).attr("x") } else { xorigin = positions[d.parent][0] } } else { xorigin = d.x } return xorigin })
 		.attr("cy", function(d) { if ((typeof d.parent !== 'undefined') && (typeof d.parent !== 'boolean')) { if (typeof positions[d.parent] === 'undefined') { xorigin = d3.select("#id"+d.parent).attr("y") } else { xorigin = positions[d.parent][1] } } else { xorigin = d.y } return xorigin })
@@ -63,23 +67,26 @@ force_graph.onmessage = function (event) {
 		.attr("cx", function(d) { return d.x })
 		.attr("cy", function(d) { return d.y })
 		.style("opacity", 1)
+
 	nu.append("title")
 			.text(function(d) { return d.name; })
+	
 	bi = node.merge(nu)
 	bi.select("text")
   		.text(function(d) {	return d.name})
    	bi.select("title")
    		.text(function(d) {	return d.name})
 };
-
 // CONSTRUCT NEW TREE
 tree = new Tree();
+
 // BELOW TREE IS FED WITH USER INPUT
 document.getElementById("submit").addEventListener("click", function(e){
 	console.log(document.getElementById("str_search").value)		
 	tree.new_node(capital_letter(document.getElementById("str_search").value))
 	tree.load_nodes("categories");
 })
+
 function capital_letter(str) 
 {
 	str = str.split(" ");
