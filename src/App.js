64bec598bd5e00a27bc0f0a4ed1b56dd39c4a434
Clément Corbin <corbin.clement@gmail.com>
2021-09-10 23:01:28 +0200

    adding & removing nodes, edges

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { useState } from "react";
 import Splash from "./components/splash.js";
+import WikiGraph from "./data-graph/graph.js";
 import "./index.css";
 
 function App() {
-  const [wt,setWT] = useState({});
+  const [nodes,setNodes] = useState([]);
+  const [edges,setEdges] = useState([]);
+  const callback = (type, action, payload) => {
+    console.log(type, action, payload);
+   if (type === "nodes") {
+     if (action === "add") {
+       const newNodes = [...nodes, payload];
+       setNodes(newNodes);
+     } else {
+       const newNodes = nodes.filter(e => e !== payload);
+     }
+   } else if (type === "edges") {
+     if (action === "add") {
+       const newNodes = [...nodes, payload];
+       setNodes(newNodes);
+     } else {
+       const newNodes = nodes.filter(e => e !== payload);
+     }
+   }
+  };
   const init = (search) => {
-    setWT(search);
+    const graph = new WikiGraph(search.lang, callback);
+    graph.createNode({
+      ns: 0,
+      title: search.searchString,
+      pageid: false
+    });
   }
 
   return (
     <main className="App">
-      <Splash handleInit={ init } visibility={ (Object.keys(wt).length === 0) } />
+      <Splash handleInit={ init } visibility={ nodes.length === 0 } />
     </main>
   );
 }
