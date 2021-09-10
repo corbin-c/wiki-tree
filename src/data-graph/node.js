b3cca3b1f109812fdc17acc0e738c1af95c9cc6e
Clément Corbin <corbin.clement@gmail.com>
2021-09-10 23:03:01 +0200

    simple nodes & edges classes

--- src/data-graph/node.js
+++ src/data-graph/node.js
@@ -1,5 +1,12 @@
-const Node = class {
-  constructor() {
+import Unit from "./unit.js";
+
+const Node = class extends Unit {
+  constructor(entity, properties) {
+    super(entity, properties);
+    this.loaded = 0; //0 means not loaded, 1 is loaded, -1 is loading
+  }
+  unlink() {
+    
   }
 };
 
