2185aaed7c221bf656e5a016063b8871076a4fe3
Clément Corbin <corbin.clement@gmail.com>
2021-09-15 21:35:19 +0200

    list adjacent nodes in drawer views

--- src/components/drawer-views/article.js
+++ src/components/drawer-views/article.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import WikiAPI from "../../wikiApi.js";
+import { useSelector } from "react-redux";
+import { adjacentNodes } from "../../reducers/graph.js";
 
 function Article(props) {
   const {
@@ -8,6 +10,21 @@ function Article(props) {
     action
   } = props;
   const [extract, setExtract] = useState("");
+  const categories = useSelector(adjacentNodes(id, "taxonomy"));
+  const links = useSelector(adjacentNodes(id, "outlink"));
+  const backlinks = useSelector(adjacentNodes(id, "backlink"));
+
+  const focus = (node) => {
+    action({ action: "focusNode", options: { node }});
+  }
+  const relations = (relationsList) => {
+    return relationsList.map(sub => {
+      return (<li key={ sub.id } onClick={ () => { focus(sub) } }>
+        { sub.name }
+      </li>)
+    });
+  }
+
   useEffect(() => {
     (async () => {
       const api = new WikiAPI(lang);
@@ -23,7 +40,7 @@ function Article(props) {
         results = results.query.pages[id];
         setExtract(results.extract);
     })()
-  }, [id]);
+  }, [id, lang]);
   /* return an extract from WP,
    * a button to expand node (eg find cateogries / links)
    * a button to delete
@@ -31,6 +48,35 @@ function Article(props) {
    */
   return (<>
     <article dangerouslySetInnerHTML={ { "__html": extract } } />
+    <article>
+      { (categories.length > 0)
+        ? (<details open="true">
+            <summary>Categories:</summary>
+            <ul>
+              { relations(categories) }
+            </ul>
+          </details>)
+        : (<></>)
+      }
+      { (links.length > 0)
+        ? (<details open="true">
+            <summary>Links:</summary>
+            <ul>
+              { relations(links) }
+            </ul>
+          </details>)
+        : (<></>)
+      }
+      { (backlinks.length > 0)
+        ? (<details open="true">
+            <summary>Backlinks:</summary>
+            <ul>
+              { relations(backlinks) }
+            </ul>
+          </details>)
+        : (<></>)
+      }
+    </article>
     <div className="buttons">
       <button title="Expand node" onClick={ () => {
         action( { action: "articlesLookup", options: { pageids: [id] } });
@@ -42,7 +88,7 @@ function Article(props) {
       }}>
         <span className="material-icons-outlined">delete</span>
       </button>
-      <a href={ "https://"+lang+".wikipedia.org/?curid="+id } target="_blank" title="View this page on Wikipedia">
+      <a href={ "https://"+lang+".wikipedia.org/?curid="+id } rel="noreferrer" target="_blank" title="View this page on Wikipedia">
         <span className="material-icons-outlined">link</span>
       </a>
     </div>
--- src/components/drawer-views/category.js
+++ src/components/drawer-views/category.js
@@ -1,9 +1,45 @@
+import { adjacentNodes } from "../../reducers/graph.js";
+import { useSelector } from "react-redux";
+
 function Category(props) {
   const {
     id,
     action
   } = props;
+  const members = useSelector(adjacentNodes(id, "taxonomy"));
+
+  const focus = (node) => {
+    action({ action: "focusNode", options: { node }});
+  }
+  const catMembers = (entity) => {
+    return members.filter(e => e.entity === entity).map(sub => {
+      return (<li key={ sub.id } onClick={ () => { focus(sub) } }>
+        { sub.name }
+      </li>)
+    });
+  }
+
   return (<>
+    <article>
+      { (members.filter(e => e.entity === "category").length > 0)
+        ? (<details open="true">
+            <summary>Categories:</summary>
+            <ul>
+              { catMembers("category") }
+            </ul>
+          </details>)
+        : (<></>)
+      }
+      { (members.filter(e => e.entity === "article").length > 0)
+        ? (<details open="true">
+            <summary>Pages:</summary>
+            <ul>
+              { catMembers("article") }
+            </ul>
+          </details>)
+        : (<></>)
+      }
+    </article>
     <div className="buttons">
       <button title="Expand node" onClick={ () => {
         action( { action: "categoriesLookup", options: { cmpageid: id } });
