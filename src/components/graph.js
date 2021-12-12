b43d8064b5fcb0031706703d7c815573d2990c08
Clément Corbin <corbin.clement@gmail.com>
2021-12-12 14:33:34 +0100

    graph worker is now useless / do not import

--- src/components/graph.js
+++ src/components/graph.js
@@ -3,8 +3,6 @@ import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import WikiAPI from "../wikiApi.js";
 
-const graphWorker = new Worker("/graphDataWorker.js");
-
 let api;
 
 const Graph = (props) => {
